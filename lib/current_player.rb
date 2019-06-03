1f056f4df5376618aa407d3e98ebfa39411f0740
Thomas Viles <thomas.c.viles.jr@gmail.com>
2019-06-03 16:35:46 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,5 @@
 def turn_count(board)
-  count = 1
+  count = 0
   board.each do |token|
     if token == "X" or token == "O"
       count += 1
@@ -9,7 +9,7 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if (turn_count(board) % 2 != 0)
+  if (turn_count(board) % 2 == 0)
     return "X"
   else
     return "O"
