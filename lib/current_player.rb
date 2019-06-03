be7b2323588cb92b7e22665d0319f92caba19613
Thomas Viles <thomas.c.viles.jr@gmail.com>
2019-06-03 16:30:55 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   count = 0
-  board.each do |postition|
-    if position == "X" or postition == "O"
+  board.each do |token|
+    if token == "X" or token == "O"
       count += 1
     end
   end
